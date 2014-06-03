2b20fe01fdc4523260c8b4ecc3c8b3cb6a756c9e
Flask <flask@Flaskui-MacBook-Air.local>
2014-06-03 23:18:42 +0900

    UserDaoTest - get test : uesrDao NullPointerException

--- src/test/java/kr/ac/jejunu/dao/UserDaoTest.java
+++ src/test/java/kr/ac/jejunu/dao/UserDaoTest.java
@@ -1,13 +1,22 @@
 package kr.ac.jejunu.dao;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.*;
+import kr.ac.jejunu.model.User;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserDaoTest {
 
+	@Autowired
+	UserDao userDao;
+	
 	@Test
-	public void failTest() {
-		fail();
+	public void get() {
+		User user = userDao.findById("pop2331");
+		assertThat(user.getId(), is("pop2331"));
+		assertThat(user.getName(), is("flask"));
+		assertThat(user.getPassword(), is("1"));
 	}
 }
