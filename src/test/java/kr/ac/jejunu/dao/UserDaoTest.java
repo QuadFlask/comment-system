015fa9b6dda6cf39234059a6f874774e12806203
Flask <flask@Flaskui-MacBook-Air.local>
2014-06-03 23:23:43 +0900

    create text-context.xml for test context configuration

--- src/test/java/kr/ac/jejunu/dao/UserDaoTest.java
+++ src/test/java/kr/ac/jejunu/dao/UserDaoTest.java
@@ -5,8 +5,13 @@ import static org.hamcrest.core.Is.*;
 import kr.ac.jejunu.model.User;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "file:src/test/java/test-context.xml")
 public class UserDaoTest {
 
 	@Autowired
@@ -19,4 +24,5 @@ public class UserDaoTest {
 		assertThat(user.getName(), is("flask"));
 		assertThat(user.getPassword(), is("1"));
 	}
+	
 }
