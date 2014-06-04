2431b0d29ad5e1c54dcfc56e877e0de05f0e0fe9
Flask <flask@Flaskui-MacBook-Air.local>
2014-06-04 15:29:23 +0900

    CommentDao extends SqlSessionDaoSupport
    rename getComments -> getCommentsByPage

--- src/main/java/kr/ac/jejunu/dao/CommentDao.java
+++ src/main/java/kr/ac/jejunu/dao/CommentDao.java
@@ -2,12 +2,14 @@ package kr.ac.jejunu.dao;
 
 import java.util.List;
 
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+
 import kr.ac.jejunu.model.Comment;
 
-public class CommentDao {
+public class CommentDao extends SqlSessionDaoSupport {
 
-	public List<Comment> getComments(int page) {
-		return null;
+	public List<Comment> getCommentsByPage(int page) {
+		return getSqlSession().selectList("commendDao.getCommentsByPage", page);
 	}
 
 }
--- src/test/java/kr/ac/jejunu/dao/CommentDaoTest.java
+++ src/test/java/kr/ac/jejunu/dao/CommentDaoTest.java
@@ -5,12 +5,16 @@ import static org.junit.Assert.*;
 import java.util.List;
 
 import kr.ac.jejunu.model.Comment;
-
 import static org.hamcrest.core.Is.*;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "file:src/test/java/test-context.xml")
 public class CommentDaoTest {
 
 	@Autowired
@@ -19,7 +23,7 @@ public class CommentDaoTest {
 	@Test
 	public void getComments() {
 		int page = 1;
-		List<Comment> comments = commentDao.getComments(page);
+		List<Comment> comments = commentDao.getCommentsByPage(page);
 		assertThat(comments.size(), is(1));
 	}
 }
